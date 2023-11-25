f0987016841961c6e1947965795838b403cb4d35
Sourav Das <iamsouravdaswade420@gmail.com>
2023-11-25 22:56:19 +0530

    js added

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,115 @@
-console.log("nomoskar pitbi");
+// // // var userName = "Sourav Das"
+// // // const age = 21
+// // // let hobby = "coding"
+
+// // // console.log(userName)
+// // // console.log(age)
+// // // console.log(hobby)
+
+// // /*
+// //  function printName(para)
+// // // {
+// // //   console.log("Hello! "+para);
+// // // }
+// // */
+
+// // // (para)=>{/*statement*/}
+
+// // // let printName=(para)=>{
+// // //   console.log("Hi! "+para)
+// // // }
+
+// // // let sayMyName = function(){
+// // //   console.log("HI HIHIIHII");
+// // // }
+
+// // // console.log(printName("Wribhu"));
+
+// // // sayMyName()
+
+// // // 1. objects
+
+// // let infos=
+// // {
+// //   userName: "Rick grimes",
+// //   age: 42,
+// //   hobby: "Killing zombies",
+// //   food: "pasta",
+// //   alert:function()
+// //   {
+// //     console.log("alert")
+// //   }
+// // }
+
+// // console.log(infos.alert())
+
+// // // 2. array
+
+// // // console.log(numbers[numbers.length-2]);
+
+// // // for(var i=1;i<=numbers.length;i++){
+// // //   console.log(numbers[numbers.length-i])
+// // // }
+
+// // // for(var i of numbers){
+// // //   console.log(i)
+// // // }
+// // const func=(para)=>
+// // {
+// //   return para+10;
+// // }
+// // // console.log(func(15));
+
+// // // let numbers=[1,2,3,4]
+// // // let newNumbers = numbers.map(el=>el+10)
+
+// // // console.log(newNumbers)
+
+// // // var x=10
+// // // x=20
+// // // console.log(x);
+
+// // function printName(){
+// //   var userName="Ayandip"
+// //   let x = 20
+// // }
+// // // console.log(name);
+
+// // let x = 10
+
+// // var  myName="Ayandip"
+
+// // console.log(window. myName);
+
+// // printName()
+
+// // function printName()
+// // {
+// //   console.log("Ayandip");
+// // }
+
+// // console.log(x); //
+// // let x=10;
+
+//   function printIt(){
+//     // let i=15;
+//     for(let c = 0; c<=20;c++){}
+
+//     // console.log(c);
+//   }
+//   printIt()
+
+// const newH2 = document.createElement("h2");
+// const div = document.getElementById("cont");
+
+// newH2.innerText = "Hi by js";
+// // div.appendNode(newH2)
+
+// setInterval(() => {
+//   const newH2 = document.createElement("h2");
+//   const div = document.getElementById("cont");
+
+//   newH2.innerText = "Hi by js";
+//   newH2.style.color="red"
+//   div.appendChild(newH2);
+// }, 2000);
---
+++ JS/app2.js
@@ -0,0 +1,47 @@
+// window.alert("Hi!")
+// confirm("Hi")
+// console.log("Hi");
+// let x = prompt("Enter your name");
+// console.log(x);
+
+// if (x) {
+//   console.log(x + " Nandi Banerjee");
+// } else {
+//   console.log("Not confirmed");
+// }
+
+// let infos=
+// {
+//   userName: "Rick grimes",
+//   age: 42,
+//   hobby: "Killing zombies",
+//   food: "pasta",
+//   alert:function()
+//   {
+
+// let a=[1,2,10,11,0]
+// console.log(a.sort());
+
+// let h1 = document.getElementById("heading_text");
+
+// let div = document.getElementById("cont");
+// h1.style.color="red"
+// h1.style.textTransform="uppercase"
+// h1.style.fontWeight="bold"
+
+// h1.innerText = "Hibru is a good boy";
+// console.dir(h1);
+// let x=10;
+
+// let newH2 = `
+// <h2> value is : ${x} </h2>
+// <h1>HI boy </h1>
+// `;
+// // let newH2 = "<h2> value is :"+ x + " </h2>";
+
+// div.innerHTML = newH2;
+
+let h1s = document.querySelector(".myh1");
+console.log(h1s);
+
+
